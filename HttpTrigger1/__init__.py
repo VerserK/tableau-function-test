41cbe280432143dbe25f0eb5a6e5e655aed944a3
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-05 15:28:58 +0700

    Update return==code '200'
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -7,3 +7,4 @@ from . import mailnotiWithSQL
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     mailnotiWithSQL.run()
+    return func.HttpResponse("", status_code=200)
