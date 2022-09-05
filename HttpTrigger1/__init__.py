4ad689967f37cdb8446c9d3deddd5dc1afae388f
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-05 13:30:27 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -3,6 +3,4 @@ import logging
 import azure.functions as func
 from . import mailnotiWithSQL
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    mailnotiWithSQL.run()
+mailnotiWithSQL.run()
