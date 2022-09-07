e3c254d76287d23cf8c798ce9a344c38a76f1488
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-07 14:30:58 +0700

    change return On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -5,6 +5,5 @@ from . import mailnotiWithSQL
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # logging.info('Python HTTP trigger function processed a request.')
-    mailnotiWithSQL.run()
-    return func.HttpResponse("", status_code=200)
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse(mailnotiWithSQL.run())
