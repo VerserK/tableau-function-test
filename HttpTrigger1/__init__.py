3ed45aa32ed250e14cf8ca3d137ddac1371c05ad
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-12 10:35:45 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 import azure.functions as func
 from . import mailnotiWithSQL
@@ -8,4 +7,4 @@ from . import mailnotiWithSQL
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     mailnotiWithSQL.run()
-    return func.HttpResponse(sys.exit(0),status_code = 200)
+    return func.HttpResponse(exit(),status_code = 200)
