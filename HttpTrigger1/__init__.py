ed3196d808102274e4a145146a5fab87435c114a
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-12 10:09:23 +0700

    exit connection

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -8,4 +8,5 @@ from . import mailnotiWithSQL
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     mailnotiWithSQL.run()
-    return func.HttpResponse(sys.exit(),status_code = 200)
+    sys.exit()
+    return func.HttpResponse(status_code = 200)
