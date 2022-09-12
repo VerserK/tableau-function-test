bdc6eee8e18cbea97c47bd2aa010cff4504de45e
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-12 13:45:51 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -6,19 +6,4 @@ from . import mailnotiWithSQL
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    enable = req.params.get('Enable')
-    if not enable:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            enable = req_body.get('enable')
-
-    if enable:
-        return func.HttpResponse(f"Hello {enable}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    return func.HttpResponse(mailnotiWithSQL.run(),status_code = 200)
