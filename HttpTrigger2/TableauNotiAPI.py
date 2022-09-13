202022485e3b90974519db5e4dd4299a46e14feb
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-13 16:51:17 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   HttpTrigger2/TableauNotiAPI.py
    	modified:   HttpTrigger2/__init__.py
    	new file:   HttpTrigger2/linenotitest.py

---
+++ HttpTrigger2/TableauNotiAPI.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 12 13:52:56 2021
+@author: methee.s
+"""
+import requests
+
+def GetViewId(dashboard):
+    server = 'https://prod-apnortheast-a.online.tableau.com/api/3.14/'
+    urlHis = server + "auth/signin"
+    headers = {"Content-Type": "application/json",
+               "Accept":"application/json"}
+    payload = { "credentials": {
+                        		"personalAccessTokenName": "MailNotify",
+                        		"personalAccessTokenSecret": "YoDlRmOITVq+clw0l2ldpw==:3ZJ14rt5oTRempSJELT10C9HBSDawgsB",
+                        		"site": {
+                        			"contentUrl": "skctableau"
+                        		}
+                }
+        }
+    res = requests.post(urlHis, headers=headers, json = payload)
+    response =  res.json()
+    token = response['credentials']['token']
+    site_id = response['credentials']['site']['id']
+  
+    url = server + '/sites/'+site_id+'/views?filter=viewUrlName:eq:' + dashboard
+    headers = {"Content-Type": "application/json",
+               "Accept":"application/json",
+               "X-Tableau-Auth": token}
+    res = requests.get(url, headers=headers, json = {})
+    response =  res.json()
+    if len(response['views']) == 0 :
+        return '','','',''
+    elif len(response['views']['view']) > 1 :
+        return response['views']['view'][0]['id'],site_id,headers,server
+    else :
+        return response['views']['view'][0]['id'],site_id,headers,server
+
+def GetImage(dashboard,Id,filterName,filterValue,LineToken,message):
+    if dashboard == 'MESSAGE':
+        LineUrl = 'https://notify-api.line.me/api/notify'
+        #LineToken = 'QDd6ExB9L9onVWb2sze4DfStpiKHB6DXTVCpV2teXEk'
+        LineHeaders = {'Authorization':'Bearer '+ LineToken}
+        payload = {'message':message}
+        resp = requests.post(LineUrl, headers=LineHeaders , data = payload)
+        print(resp)
+    else:
+        view_id,site_id,headers,server = GetViewId(dashboard)
+        if Id != '':
+            view_id = Id
+        if view_id != '':
+            if filterName == '':
+                url = server +  'sites/'+site_id+'/views/'+view_id+'/image' + '?maxAge=1'+'&resolution=high'
+                print(url)
+            else:
+                url = server +  'sites/'+site_id+'/views/'+view_id+'/image' + '?vf_'+filterName+'='+filterValue+'&maxAge=1'+'&resolution=high&sort=ส่วน:asc'
+                print(url)
+            #url = server +  '/sites/'+site_id+'/views/'+view_id+'/image' + '?vf_สายงาน=สายงานวางแผนและควบคุม/CFO'
+            res = requests.get(url, headers=headers, json = {})
+            print(view_id)
+            #Send to Line
+            LineUrl = 'https://notify-api.line.me/api/notify'
+            #LineToken = 'QDd6ExB9L9onVWb2sze4DfStpiKHB6DXTVCpV2teXEk'
+            LineHeaders = {'Authorization':'Bearer '+ LineToken}
+            payload = {'message':message}
+            file = {'imageFile':res.content}
+            resp = requests.post(LineUrl, headers=LineHeaders , data = payload , files = file)
+            print(resp)
