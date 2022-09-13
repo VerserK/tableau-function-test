afcf2b4971fb6f551c87c774f3f0f7f11a4bc6ac
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-13 15:17:51 +0700

    Create Http Trigger 2 in Azure Function

---
+++ HttpTrigger2/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    DashboardName = req.params.get('DashboardName')
+    if not DashboardName:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            DashboardName = req_body.get('name')
+
+    if DashboardName:
+        return func.HttpResponse(f"Hello, {DashboardName}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
