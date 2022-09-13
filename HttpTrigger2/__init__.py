4105813c118599c46fab98119c09d930c1195b90
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-13 15:42:04 +0700

    Allow Api to Server Azure

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -7,6 +7,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     DashboardName = req.params.get('DashboardName')
+    ViewId = req.params.get('ViewId')
+    Token = req.params.get('Token')
+    FilterName = req.params.get('FilterName')
+    FilterValue = req.params.get('FilterValue')
+    Time = req.params.get('Time')
+    Message = req.get('Message')
     if not DashboardName:
         try:
             req_body = req.get_json()
@@ -16,7 +22,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             DashboardName = req_body.get('DashboardName')
 
     if DashboardName:
-        return func.HttpResponse(f"Hello, {DashboardName}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {DashboardName}. {ViewId} {Token} {FilterName} {FilterValue} {Time} {Message}This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
