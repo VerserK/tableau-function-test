1d076e70d48a61e9e0af3bd4c32c44253af23095
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-14 10:09:19 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HttpTrigger2/__init__.py
    	modified:   HttpTrigger2/linenotitest.py

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -1,6 +1,17 @@
 import logging
 
 import azure.functions as func
+import pickle
+import pandas as pd
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from datetime import datetime, timedelta
+from . import TableauNotiAPI as tbn
+from croniter import croniter
+import os
+import tempfile
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -19,6 +30,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             DashboardName = req_body.get('DashboardName')
+
+    #Convert to Pandas Dataframe
+    df = pd.DataFrame({
+    'DashboardName' : [DashboardName],
+    'ViewId' : [ViewId],
+    'Token' : [Token],
+    'FilterName' : [FilterName],
+    'FilterValue' : [FilterValue],
+    'Time' : [Time],
+    'Message' : [Message]
+    })
+    df.columns = ['DashboardName','ViewId','Token','FilterName','FilterValue','Message']
+    for index, row in df.iterrows():
+        if row['DashboardName'] != '':
+            tbn.GetImage(row['DashboardName'],row['ViewId'],row['FilterName'],row['FilterValue'],row['Token'],row['Message'])
 
     if DashboardName:
         return func.HttpResponse(f"Hello, {DashboardName}. {ViewId} {Token} {FilterName} {FilterValue} {Time} {Message}")
