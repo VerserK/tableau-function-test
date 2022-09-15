dd7abb36d560ed0c2c48d2ebc7f2ed4287ce2b13
akarawatp <akarawat.p@siamkubota.co.th>
2022-09-15 10:39:07 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HttpTrigger1/__init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -52,7 +52,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     'Subject' : Subject,
     'Content' : Content
     })
-    df.drop(df[df.Enable_list != 'x'].index, inplace=True)
+    df.drop(df[df.Enable != 'x'].index, inplace=True)
     print(df)
     # groups = df.groupby('MailGroup')
     # for name, group in groups:
