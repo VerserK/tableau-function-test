514f306d6a2c97360e4a3c4f47af3dddabe77aca
akarawatp <akarawat.p@siamkubota.co.th>
2022-11-11 19:55:15 +0700

    Upload file

---
+++ Crisis-noti/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    eid = req.params.get('eid')
+    EmergencyNotify_NS.main(eid)
+
+    if eid:
+        return func.HttpResponse(f"Hello, {eid}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
