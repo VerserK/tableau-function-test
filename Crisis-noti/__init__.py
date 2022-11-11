2a2b25fe8cd9cb567a1bd1283c0b843a13ac8290
akarawatp <akarawat.p@siamkubota.co.th>
2022-11-11 19:55:45 +0700

    from . import EmergencyNotify_NS

--- Crisis-noti/__init__.py
+++ Crisis-noti/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 import azure.functions as func
-
+from . import EmergencyNotify_NS
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
