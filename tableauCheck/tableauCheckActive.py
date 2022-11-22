2f2a776c7fe24076b13290ff11174247af47bfe8
akarawatp <akarawat.p@siamkubota.co.th>
2022-11-22 09:36:53 +0700

    Tableau Check Activate

---
+++ tableauCheck/tableauCheckActive.py
@@ -0,0 +1,58 @@
+from pymysql import NULL
+from tableau_api_lib import TableauServerConnection
+from tableau_api_lib.utils import querying , flatten_dict_column
+from datetime import datetime, timezone, timedelta
+import pandas as pd
+import sqlalchemy as sa
+import urllib
+from sqlalchemy.sql import text as sa_text
+import requests
+import numpy as np
+def run():
+        ### Dev01 Connection ###
+        server1 = 'dwhsqldev01.database.windows.net'
+        database1 = 'Tableau Data'
+        username1 = 'boon'
+        password1 = 'DEE@DA123'
+        driver1 = '{ODBC Driver 17 for SQL Server}'
+        dsn1 = 'DRIVER='+driver1+';SERVER='+server1+';PORT=1433;DATABASE='+database1+';UID='+username1+';PWD='+ password1
+
+        params1 = urllib.parse.quote_plus(dsn1)
+        engine1 = sa.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params1)
+        conn1 = engine1.connect()
+
+        tableau_server_config = {
+                'my_env': {
+                        'server': 'https://prod-apnortheast-a.online.tableau.com',
+                        'api_version': '3.17',
+                        'personal_access_token_name': 'TestToken',
+                        'personal_access_token_secret': '5830C81FQmyACKTsgeyGjQ==:w3WLdOoielNFNbsYkcUO5ejkZruZq9x0',
+                        'site_name': 'skctableau',
+                        'site_url': 'skctableau'
+                }
+        }
+
+        conn = TableauServerConnection(tableau_server_config, env='my_env')
+        conn.sign_in()
+        user_df = querying.get_users_dataframe(conn)
+        user_df["lastLogin"] = pd.to_datetime(user_df["lastLogin"]).dt.strftime('%Y-%m-%d')
+        user_df["lastLogin"] = pd.to_datetime(user_df["lastLogin"], format='%Y-%m-%d')
+
+        ## Email Check
+        ec = pd.read_sql("SELECT * FROM tableau_83_sendmail", conn1)
+        ecc = user_df[user_df['email'].isin(ec['email'])]
+        ecc = ecc[~ecc['lastLogin'].isin(user_df['lastLogin'])]
+        ###### Line Noti Message #####
+        LineUrl = 'https://notify-api.line.me/api/notify'
+        LineToken = 'XVDGomv0AlT1oztR2Ntyad7nWUYvBWU7XLHPREQYm6e'
+        LineHeaders = {'Authorization':'Bearer '+ LineToken}
+        if ecc.empty:
+                print('DataFrame is empty!')
+        else:
+                payload=[]
+                for index, row in ecc.iterrows():
+                        msg = 'Tableau is activate : '
+                        payload.insert(index,row['email'])
+                payload = ','.join(payload)
+                print(payload)
+                resp = requests.post(LineUrl, headers=LineHeaders , data = {'message' : msg+payload})
