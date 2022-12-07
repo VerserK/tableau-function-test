9eb24842754ac8952c67adb4983b168f7a20ed32
akarawatp <akarawat.p@siamkubota.co.th>
2022-12-07 15:53:45 +0700

    update Function

--- PartStockTimer/connectDWHwithpath.py
+++ PartStockTimer/connectDWHwithpath.py
@@ -1,23 +1,19 @@
 import pandas as pd
 # from azure.storage.blob import BlobServiceClient, __version__
 
-# CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=dwhwebstorage;AccountKey=A8aP+xOBBD5ahXo9Ch6CUvzsqkM5oyGn1/R3kcFcNSrZw4aU0nE7SQCBhHQFYif1AEPlZ4/pAoP/+AStKRerPQ==;EndpointSuffix=core.windows.net"
-# CONTAINERNAME = "partsdwh-log"
-# BLOBNAME = "PartsDWHLog.xlsx"
-# LOCALFILENAME = "PartsDWHLog.xlsx" 
-
-# blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
-# container_client = blob_service_client.get_container_client(CONTAINERNAME)
-# blob_client = blob_service_client.get_blob_client(container = CONTAINERNAME, blob=BLOBNAME)
-
-# #READ PRODUCTS FILE
-# f = open(LOCALFILENAME, "wb")
-# f.write(blob_client.download_blob().content_as_bytes())
-# f.close()
-# df = pd.read_excel(r''+LOCALFILENAME)
-# print(df)
-
-# df = pd.read_csv(r"\\178.50.0.170\E\MyName\test.csv")
 def run():
-    df = pd.read_excel(r'\\172.31.1.98\Z\PartsDWH_Log\PartsDWHLog.xlsx',index_col='Table')
-    print(df)
+    CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=dwhwebstorage;AccountKey=A8aP+xOBBD5ahXo9Ch6CUvzsqkM5oyGn1/R3kcFcNSrZw4aU0nE7SQCBhHQFYif1AEPlZ4/pAoP/+AStKRerPQ==;EndpointSuffix=core.windows.net"
+    CONTAINERNAME = "dwhdart"
+    BLOBNAME = "PartField_to_Map.csv"
+    LOCALFILENAME = "PartField_to_Map.csv" 
+
+    blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(CONTAINERNAME)
+    blob_client = blob_service_client.get_blob_client(container = CONTAINERNAME, blob=BLOBNAME)
+
+    #READ PRODUCTS FILE
+    f = open(LOCALFILENAME, "wb")
+    f.write(blob_client.download_blob().content_as_bytes())
+    f.close()
+    df_cols = pd.read_csv(r''+LOCALFILENAME)
+    print(df_cols)
--- PartStockTimer/fix_dartstock.py
+++ PartStockTimer/fix_dartstock.py
@@ -70,8 +70,21 @@ def uploadCSV(data, filepath,table,FOLDER,dsn):
     cursor = mydb.cursor()
     
     df = data
-    
-    df_cols = pd.read_csv(r"D:\DWH\DART\PartField_to_Map.csv")
+
+    CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=dwhwebstorage;AccountKey=A8aP+xOBBD5ahXo9Ch6CUvzsqkM5oyGn1/R3kcFcNSrZw4aU0nE7SQCBhHQFYif1AEPlZ4/pAoP/+AStKRerPQ==;EndpointSuffix=core.windows.net"
+    CONTAINERNAME = "dwhdart"
+    BLOBNAME = "PartField_to_Map.csv"
+    LOCALFILENAME = "PartField_to_Map.csv" 
+
+    blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(CONTAINERNAME)
+    blob_client = blob_service_client.get_blob_client(container = CONTAINERNAME, blob=BLOBNAME)
+
+    #READ PRODUCTS FILE
+    f = open(LOCALFILENAME, "wb")
+    f.write(blob_client.download_blob().content_as_bytes())
+    f.close()
+    df_cols = pd.read_csv(r''+LOCALFILENAME)
     
     for col in df.columns:
         
