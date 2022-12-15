4d3347a5beec0f01f83a0248eff69362a0e02fbd
akarawatp <akarawat.p@siamkubota.co.th>
2022-12-15 09:10:12 +0700

    test

--- VCCTimer/SFTPconnect.py
+++ VCCTimer/SFTPconnect.py
@@ -22,76 +22,76 @@ def run():
         print("Connection successfully established ... ")
         # Switch to a remote directory
         # sftp.cwd('/dnscallcenter/')
-        dnscallcenter = '/dnscallcenter/'
-        archive_dir = '/archivefile/'
-        today = datetime.date.today()
-        today = str(today)
-        # print(today)
-        # Obtain structure of the remote directory '/opt'
-        directory_structure = sftp.listdir_attr()
+        # dnscallcenter = '/dnscallcenter/'
+        # archive_dir = '/archivefile/'
+        # today = datetime.date.today()
+        # today = str(today)
+        # # print(today)
+        # # Obtain structure of the remote directory '/opt'
+        # directory_structure = sftp.listdir_attr()
 
-        # Print data
-        for attr in directory_structure:
-            if attr.filename.endswith("xlsx"):
-                file = attr.filename
-                fname = file.split('.')
-                fname.insert(0,'VCCReport'+today)
-                fname.remove('VCCReport')
-                fname = '.'.join(fname)
-                print(fname)
-                print(file)
-                sftp.rename(dnscallcenter + file, archive_dir + fname)
+        # # Print data
+        # for attr in directory_structure:
+        #     if attr.filename.endswith("xlsx"):
+        #         file = attr.filename
+        #         fname = file.split('.')
+        #         fname.insert(0,'VCCReport'+today)
+        #         fname.remove('VCCReport')
+        #         fname = '.'.join(fname)
+        #         print(fname)
+        #         print(file)
+        #         sftp.rename(dnscallcenter + file, archive_dir + fname)
 
-        print('/archivefile/VCCReport'+today+'.xlsx')
-        with sftp.open('/archivefile/VCCReport'+today+'.xlsx', 'r', bufsize=32768) as f:
-            raw_df = pd.read_excel(f,header=None)
+        # print('/archivefile/VCCReport'+today+'.xlsx')
+        # with sftp.open('/archivefile/VCCReport'+today+'.xlsx', 'r', bufsize=32768) as f:
+        #     raw_df = pd.read_excel(f,header=None)
 
-            col_name = ['Agent','Date','Time','Logon duration','Total pause duration (any)',
-            'Break','Callout','Follow Up Case','Lunch','Meeting','Ronar','Toilet','Wrapup duration',
-            'Ring duration total','Ring duration avg','Ring duration min','Ring duration max',
-            'Idle duration total' , 'CCIN Ext','CCOUT Ext','Calls Total','Calls Serviced','Conversation duration sum',
-            'Conversation duration avr']
+        #     col_name = ['Agent','Date','Time','Logon duration','Total pause duration (any)',
+        #     'Break','Callout','Follow Up Case','Lunch','Meeting','Ronar','Toilet','Wrapup duration',
+        #     'Ring duration total','Ring duration avg','Ring duration min','Ring duration max',
+        #     'Idle duration total' , 'CCIN Ext','CCOUT Ext','Calls Total','Calls Serviced','Conversation duration sum',
+        #     'Conversation duration avr']
 
-            df_list = np.split(raw_df, raw_df[raw_df.isnull().all(1)].index)
+        #     df_list = np.split(raw_df, raw_df[raw_df.isnull().all(1)].index)
 
-            count = 1
-            df = df_list[1]
-            df = df[4:]
-            df = df.reset_index(drop=True)
-            df = df.dropna(how='all',axis=1)
-            df.columns = col_name
+        #     count = 1
+        #     df = df_list[1]
+        #     df = df[4:]
+        #     df = df.reset_index(drop=True)
+        #     df = df.dropna(how='all',axis=1)
+        #     df.columns = col_name
 
-            df['Datetime'] = df['Date'].astype(str).str.slice(0,10)+' '+df['Time'].astype(str)
-            df.drop(columns=['Date','Time'],inplace=True)
+        #     df['Datetime'] = df['Date'].astype(str).str.slice(0,10)+' '+df['Time'].astype(str)
+        #     df.drop(columns=['Date','Time'],inplace=True)
 
-            for c in df.columns:
-                if c == 'Agent':
-                    continue
-                elif c == 'Datetime':
-                    df[c] = pd.to_datetime(df[c].astype(str))
-                elif c in ['CCIN Ext','CCOUT Ext','Calls Total','Calls Serviced']:
-                    df[c] = pd.to_numeric(df[c].astype(str))
-                else:
-                    df[c] = pd.to_timedelta(df[c].astype(str))
-                    df[c] = df[c]/ np.timedelta64(1, 'h')
+        #     for c in df.columns:
+        #         if c == 'Agent':
+        #             continue
+        #         elif c == 'Datetime':
+        #             df[c] = pd.to_datetime(df[c].astype(str))
+        #         elif c in ['CCIN Ext','CCOUT Ext','Calls Total','Calls Serviced']:
+        #             df[c] = pd.to_numeric(df[c].astype(str))
+        #         else:
+        #             df[c] = pd.to_timedelta(df[c].astype(str))
+        #             df[c] = df[c]/ np.timedelta64(1, 'h')
                     
-            df = df[['Agent', 'Datetime', 'Logon duration', 'Total pause duration (any)', 'Break',
-                'Callout', 'Follow Up Case', 'Lunch', 'Meeting', 'Ronar', 'Toilet',
-                'Wrapup duration', 'Ring duration total', 'Ring duration avg',
-                'Ring duration min', 'Ring duration max', 'Idle duration total',
-                'Calls Total', 'Calls Serviced','Conversation duration sum', 'Conversation duration avr']]
+        #     df = df[['Agent', 'Datetime', 'Logon duration', 'Total pause duration (any)', 'Break',
+        #         'Callout', 'Follow Up Case', 'Lunch', 'Meeting', 'Ronar', 'Toilet',
+        #         'Wrapup duration', 'Ring duration total', 'Ring duration avg',
+        #         'Ring duration min', 'Ring duration max', 'Idle duration total',
+        #         'Calls Total', 'Calls Serviced','Conversation duration sum', 'Conversation duration avr']]
 
-            #### optional ####
-        with sftp.open('/transform/transform'+today+'.csv', 'w') as f:
-            df.to_csv(f,index=False)
+        #     #### optional ####
+        # with sftp.open('/transform/transform'+today+'.csv', 'w') as f:
+        #     df.to_csv(f,index=False)
 
-        server = r'SKCDWH01' #172.31.8.25    
-        database = 'Voxtron_Callcenter' 
-        username = 'boon'
-        password = 'Boon@DA123'
-        driver = '{ODBC Driver 17 for SQL Server}'
-        dsn = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
-        params = urllib.parse.quote_plus(dsn)                    
-        engine = sa.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
-        conn = engine.connect()
-        df.to_sql("VCC_logs", con=conn,if_exists = 'append', index=False, schema="dbo")
+        # server = r'SKCDWH01' #172.31.8.25    
+        # database = 'Voxtron_Callcenter' 
+        # username = 'boon'
+        # password = 'Boon@DA123'
+        # driver = '{ODBC Driver 17 for SQL Server}'
+        # dsn = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
+        # params = urllib.parse.quote_plus(dsn)                    
+        # engine = sa.create_engine('mssql+pyodbc:///?odbc_connect=%s' % params)
+        # conn = engine.connect()
+        # df.to_sql("VCC_logs", con=conn,if_exists = 'append', index=False, schema="dbo")
