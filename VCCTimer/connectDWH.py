4c95af19d38506ee346e735ff9d4bd16a114b10c
akarawatp <akarawat.p@siamkubota.co.th>
2022-12-16 10:41:13 +0700

    config user pwd

--- VCCTimer/connectDWH.py
+++ VCCTimer/connectDWH.py
@@ -3,6 +3,8 @@ def test():
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                         'Server=172.31.8.25;'
                         'Database=Voxtron_Callcenter;'
+                        'UID=boon;'
+                        'PWD=Boon@DA123;'
                         'Trusted_Connection=yes;')
 
     cursor = conn.cursor()
