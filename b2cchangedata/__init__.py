ad0f23a8ff5ae6019eac6248786ee011af2fdb66
akarawatp <akarawat.p@siamkubota.co.th>
2023-06-01 15:34:38 +0700

    qweqaaa

--- b2cchangedata/__init__.py
+++ b2cchangedata/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import azure.functions as func
+import json
 from . import sendmail
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -16,7 +17,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if data:
         # sendmail.gmail_send_message(userid,fullname,status,oldnumber,newnumber,email)
-        return func.HttpResponse(f"Hello, {req_body}. successfully.", status_code = 200)
+        return func.HttpResponse(f"Hello, {json.dumps(data)}. successfully.", status_code = 200)
     else:
         return func.HttpResponse(
              "Fail not pass parameter fullname in url",
