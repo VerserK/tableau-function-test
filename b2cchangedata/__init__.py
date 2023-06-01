2bcc9cd67858a22b846567ad918a18df7b091345
akarawatp <akarawat.p@siamkubota.co.th>
2023-06-01 15:12:52 +0700

    testsetset

--- b2cchangedata/__init__.py
+++ b2cchangedata/__init__.py
@@ -4,12 +4,7 @@ from . import sendmail
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    userid = req.get_body('userid')
-    fullname = req.get_body('fullname')
-    status = req.get_body('status')
-    oldnumber = req.get_body('oldnumber')
-    newnumber = req.get_body('newnumber')
-    email = req.get_body('email')
+    data = req.get_json()
 
     if not fullname:
         try:
@@ -20,8 +15,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             fullname = req_body.get('fullname')
 
     if fullname:
-        sendmail.gmail_send_message(userid,fullname,status,oldnumber,newnumber,email)
-        return func.HttpResponse(f"Hello, {fullname}. successfully.", status_code = 200)
+        # sendmail.gmail_send_message(userid,fullname,status,oldnumber,newnumber,email)
+        return func.HttpResponse(f"Hello, {data}. successfully.", status_code = 200)
     else:
         return func.HttpResponse(
              "Fail not pass parameter fullname in url",
