74052b566fb8e79a9c582016a6ca47d568e82693
akarawatp <akarawat.p@siamkubota.co.th>
2023-08-30 14:39:38 +0700

    ะำหะ

--- MailNoti/mailnotiWithSQL.py
+++ MailNoti/mailnotiWithSQL.py
@@ -327,10 +327,10 @@ def send_message(user_id, message):
   # time.
 
   # Create the BlobServiceClient object which will be used to create a container client
-  blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=d710rgsi01diag;AccountKey=nr/2Yn9nN9bWr0GNNSiNvBbN91MfYpkcIK0+9xcrYMdrFttcEAqV4kBBGGd8ehk+BRZ0gfe0iOTeoYVlRNbXOw==;EndpointSuffix=core.windows.net')
+  blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=dwhwebstorage;AccountKey=A8aP+xOBBD5ahXo9Ch6CUvzsqkM5oyGn1/R3kcFcNSrZw4aU0nE7SQCBhHQFYif1AEPlZ4/pAoP/+AStKRerPQ==;EndpointSuffix=core.windows.net')
 
   # Create a unique name for the container
-  container_name = 'methee-google-file'
+  container_name = 'google-file'
 
   # Create a blob client using the local file name as the name for the blob
   blob_client = blob_service_client.get_blob_client(container=container_name, blob='gmail-tableau-token.json')
